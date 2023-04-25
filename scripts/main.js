f1aea1aea7690006902968199f715e15023e166f
Pedro-No <pedr0.nogueira@hotmail.com>
2023-04-25 09:40:50 +0100

    added button press

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@ console.log('hello vi and p')
 const canvas = document.getElementById("canvas"); //canvas
 const ctx = canvas.getContext("2d"); //2d
 
+const buttonL = document.querySelector('.left-bttn') //buttons Left
+const buttonR = document.querySelector('.right-bttn')
+const buttonU = document.querySelector('.up-bttn')
+const buttonD = document.querySelector('.down-bttn')
+
 const player = new Sprite(360, 210, characterImg); // creates new object for the characterimg        
 const background = new Sprite(250, -180, backgroundImg); // creates new object for the backgroundimg    
 const bomb = new Sprite(360, 430, bombImg); // creates new object for the bomnimg
@@ -22,24 +27,48 @@ window.onload = () => {
   document.addEventListener("keydown", (e) => {                // creates event listener for key press to move the car
     switch (e.key) {
       case "ArrowUp":
-        game.moveUp()
+        game.moveUp();
+        buttonPress(buttonU)
       break;
       case "ArrowLeft":
-        game.moveLeft()
+        game.moveLeft();
+        buttonPress(buttonL);
       break;
       case "ArrowRight":
-        game.moveRight()
+        game.moveRight();
+        buttonPress(buttonR);
       break;
       case "ArrowDown":
-        game.moveDown()
+        game.moveDown();
+        buttonPress(buttonD);
+      break;
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    switch (e.key) {
+      case "ArrowUp":
+        buttonDepress(buttonU)
+      break;
+      case "ArrowLeft":
+        buttonDepress(buttonL);
+      break;
+      case "ArrowRight":
+        buttonDepress(buttonR);
+      break;
+      case "ArrowDown":
+        buttonDepress(buttonD);
       break;
     }
   });
 }
 
-/* Keys
-const htmlUp = document.getElementById("up-bttn");
-const htmlDown = document.getElementById("down-bttn");
-const htmlLeft = document.getElementById("left-bttn");
-const htmlRight = document.getElementById("right-bttn");
-*/
+function buttonPress(button) {  
+  button.classList.remove("button-style-Depressed");
+  button.classList.add("button-style-Pressed");
+}
+
+function buttonDepress(button) {
+  button.classList.remove("button-style-Pressed");
+  button.classList.add("button-style-Depressed");
+}
